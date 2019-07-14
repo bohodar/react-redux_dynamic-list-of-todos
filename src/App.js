18c22fb9a7206a26624bfe9562a28bf5cdad382f
Bohodar <bodik264478@gmail.com>
2019-07-14 12:51:29 +0300

    Initialize commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h3> Ready to work</h3>
+  );
+}
+
+export default App;
+
+
+// <img src={logo} className="App-logo" alt="logo" /> //
